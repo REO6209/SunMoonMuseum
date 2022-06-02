ba8f494fd0d8dbe1b8fda9a80841378d3f708882
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-06-02 14:51:53 +0900

    Delete SMMuseum/SMMuseum directory

--- SMMuseum/SMMuseum/testapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from testapp.models import TestOCR
-
-
-class TestCreationForm(ModelForm):
-    class Meta:
-        model = TestOCR
-        fields = ['title', 'image']
--- SMMuseum/SMMuseum/testapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TestOCR(models.Model):
-    title = models.CharField(max_length=200, null=True)
-    image = models.ImageField(upload_to='test/', null=False)
